b7714080a11a7c0a00a32b63287054c2ed6653b7
Andre Warken <andrekw@gmail.com>
2019-06-08 07:56:31 -0400

    refactored model

--- few_shot/model.py
+++ few_shot/model.py
@@ -1,9 +1,11 @@
+from typing import Tuple
+
 import tensorflow as tf
 tf.enable_eager_execution()
-from tensorflow.keras.layers import Conv2D, BatchNormalization, ReLU, MaxPooling2D, Input, Flatten, Lambda, Softmax
+from tensorflow.keras.layers import Conv2D, BatchNormalization, ReLU, MaxPooling2D, Input, Flatten, Lambda, Softmax, Layer
 from tensorflow.keras.models import Model
 
-from .dataset import OmniglotDataset
+from few_shot.dataset import OmniglotDataset
 
 N_SHOT = 1
 K_WAY = 60
@@ -15,41 +17,63 @@ train_it = train_ds.tf_iterator()
 n_classes = train_ds.df.class_id.nunique()
 
 # first retrieve embeddings
-embedding_input = Input(shape=(28, 28, 1))
+img_shape = (28, 28, 1)
 
-embedding = Conv2D(64, 3, data_format='channels_last', padding='same')(embedding_input)
-embedding = BatchNormalization()(embedding)
-embedding = ReLU()(embedding)
-embedding = MaxPooling2D(pool_size=(2, 2))(embedding)
-embedding = Conv2D(64, 3, data_format='channels_last', padding='same')(embedding)
-embedding = BatchNormalization()(embedding)
-embedding = ReLU()(embedding)
-embedding = MaxPooling2D(pool_size=(2, 2))(embedding)
-embedding = Conv2D(64, 3, data_format='channels_last', padding='same')(embedding)
-embedding = BatchNormalization()(embedding)
-embedding = ReLU()(embedding)
-embedding = MaxPooling2D(pool_size=(2, 2))(embedding)
-embedding = Conv2D(64, 3, data_format='channels_last', padding='same')(embedding)
-embedding = BatchNormalization()(embedding)
-embedding = ReLU()(embedding)
-embedding = MaxPooling2D(pool_size=(2, 2))(embedding)
-embedding = Flatten()(embedding)
-embedding_model = Model(embedding_input, embedding)
+def build_embedding_model(input_layer: Layer, n_convs=4):
+    """Builds an embedding model as described in the Prototypical Networks paper."""
+    embedding = input_layer  # need to keep a reference to the input
+    for _ in range(n_convs):
+        embedding = Conv2D(64, 3, data_format='channels_last', padding='same')(embedding)
+        embedding = BatchNormalization()(embedding)
+        embedding = ReLU()(embedding)
+        embedding = MaxPooling2D(pool_size=(2, 2))(embedding)
+    embedding = Flatten()(embedding)
+    model = Model(input_layer, embedding)
 
-support_in = Input(shape=(28, 28, 1), name='support_input')
-query_in = Input(shape=(28, 28, 1), name='query_input')
+    return model
+
+
+def centroids(support_embedding: Layer, support_labels: Layer, n_shot: int):
+    """Computes class centroids as the class mean.
+
+    :params
+    support_embedding: layer containing the embeddings
+    support_labels: one-hot encoded class labels
+    n_shot: number of examples per class."""
+    
+    return tf.matmul(support_embedding, support_labels, transpose_a=True) / n_shot
+
+
+def negative_distance(query_embedding: Layer, class_centroids: Layer):
+    """Computes the negative squared euclidean distance between each query point and each class centroid."""
+
+    # we need to expand the embedding tensor in order for broadcasting to work
+    # our query tensor shape is (q_queries, embedding_dims), and the centroids (embedding_dims, n_classes)
+    # our output should be (q_queries, embedding_dims, n_classes)
+    query_embedding = tf.expand_dims(query_embedding, axis=-1)
+    sq_distance = tf.squared_difference(query_embedding, class_centroids)
+
+    # reduce over the embedding dimension to find the distance between each query and class centroid
+    # we return the negative distance since we want to activate the closest centroid, not the farthest (softmin?)
+    return -tf.reduce_sum(sq_distance, axis=1)
+
+embedding_in = Input(shape=img_shape)
+embedding_model = build_embedding_model(embedding_in)
+
+
+support_in = Input(shape=img_shape, name='support_input')
+query_in = Input(shape=img_shape, name='query_input')
 support_labels = Input(shape=(n_classes,), name='support_labels')
 
 support_embedding = embedding_model(support_in)
 query_embedding = embedding_model(query_in)
 
-class_centroids = Lambda(lambda x: tf.matmul(x[0], x[1], transpose_a=True) / N_SHOT)([support_embedding, support_labels])
+# Lambda layers only accept a sequence of tensors as input
+class_centroids = Lambda(lambda x: centroids(*x, N_SHOT))((support_embedding, support_labels))
 
-distances = Lambda(lambda x: -tf.reduce_sum(
-    tf.squared_difference(tf.expand_dims(x[0], axis=-1), x[1]), axis=1))([query_embedding, class_centroids])
-# negate distance because we want to have the smallest distance as the max (softmin?)
+negative_distances = Lambda(lambda x: negative_distance(*x))((query_embedding, class_centroids))
 
-predictions = Softmax()(distances)
+predictions = Softmax()(negative_distances)
 
 model = Model(inputs=[support_in, support_labels, query_in], outputs=predictions)
 
