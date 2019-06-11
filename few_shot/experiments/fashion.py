dae58065d127fe92f1678f73ad86e3cee5ad0fee
Andre Kw <andre@andrekw.com>
2019-06-11 20:51:52 +0000

    fashion experiment according to paper

--- few_shot/experiments/fashion.py
+++ few_shot/experiments/fashion.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Tuple
 
 import numpy as np
@@ -100,8 +101,8 @@ def evaluate_fashion_few_shot(train_df,
     val_dataset = FewShotEpisodeGenerator(val_df[['class_name', 'filepath']].copy(),
                                           n_epochs * eps_per_epoch,
                                           n_shot,
-                                          k_way_test,
-                                          n_queries_test)
+                                          k_way_train,
+                                          n_queries_train)
 
     test_dataset = FewShotEpisodeGenerator(test_df[['class_name', 'filepath']].copy(),
                                            n_epochs * eps_per_epoch,
@@ -110,13 +111,13 @@ def evaluate_fashion_few_shot(train_df,
                                            n_queries_test)
 
     train_it = train_dataset.tf_iterator(image_pipeline=resize_img_pipeline_fn(img_shape))
-    val_it = val_dataset.tf_iterator(image_pipeline=resize_img_pipeline_fn(img_shape),
-                                     post_transform=pad_validation_inputs(n_shot,
+    val_it = val_dataset.tf_iterator(image_pipeline=resize_img_pipeline_fn(img_shape)) #,
+    '''                                 post_transform=pad_validation_inputs(n_shot,
                                                                           n_queries_train,
                                                                           n_queries_test,
                                                                           k_way_train,
                                                                           k_way_test))
-
+    '''
     embedding_input = tf.keras.layers.Input(shape=img_shape)
     embedding_model = build_embedding_model(embedding_input)
     model = build_prototype_network(n_shot,
@@ -136,7 +137,9 @@ def evaluate_fashion_few_shot(train_df,
               validation_steps=eps_per_epoch,
               callbacks=[
                   tf.keras.callbacks.LearningRateScheduler(
-                      lambda i, lr: lr if i % (2000//eps_per_epoch) else lr * 0.5)])
+                      lambda i, lr: lr if i % (2000//eps_per_epoch) else lr * 0.5),
+                  tf.keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
+              ])
     test_it = test_dataset.tf_iterator(image_pipeline=resize_img_pipeline_fn(img_shape))
 
     test_model = build_prototype_network(n_shot,
@@ -148,3 +151,56 @@ def evaluate_fashion_few_shot(train_df,
     test_model.compile(optimizer=test_opt, loss='categorical_crossentropy', metrics=['categorical_accuracy'])
     test_loss, test_acc = test_model.evaluate(test_it, steps=test_eps)
     return test_loss, test_acc
+
+
+if __name__ == '__main__':
+    tf.enable_eager_execution()
+
+    np.random.seed(23)
+    tf.random.set_random_seed(29)
+
+    SHOTS = [1, 5]
+    TEST_K_WAY = [5, 20]
+
+    lr = 1e-3
+    n_queries_train = 5
+    n_queries_test = 5
+    k_way_train = 25
+    eps_per_epoch = 100
+    n_epochs = 100
+    test_eps = 1000
+    img_shape = (40, 30, 3)
+
+    train_df, val_df, test_df = fashion_dfs('datasets/fashion_mac/fashion-dataset', n_val_classes=30)
+
+    results = []
+    for n_shots, k_way_test in itertools.product(SHOTS, TEST_K_WAY):
+        print(f'Running fashion experiment {n_shots}-shot, {k_way_test} way')
+        test_loss, test_acc = evaluate_fashion_few_shot(train_df=train_df,
+                                                        val_df=val_df,
+                                                        test_df=test_df,
+                                                        lr = lr,
+                                                        n_shot = n_shots,
+                                                        n_queries_train = n_queries_train,
+                                                        n_queries_test = n_queries_test,
+                                                        k_way_train = k_way_train,
+                                                        eps_per_epoch = eps_per_epoch,
+                                                        n_epochs = n_epochs,
+                                                        k_way_test = k_way_test,
+                                                        test_eps = test_eps,
+                                                        img_shape = (40, 30, 3))
+
+        results.append({
+            'n_shots': n_shots,
+            'k_way_train': k_way_train,
+            'k_way_test': k_way_test,
+            'learning_rate': lr,
+            'n_queries': n_queries_test,
+            'n_queries_train': n_queries_train,
+            'test_accuracy': test_acc,
+            'test_loss': test_loss
+        })
+
+    df = pd.DataFrame.from_records(results)
+    print(df)
+    df.to_csv('fashion_results.csv')
