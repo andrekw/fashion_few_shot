3fd7b3616317f51cd46e023340a304aa87d43984
Andre KW <andrekw@gmail.com>
2019-06-21 21:15:21 +0200

    new experiments

--- few_shot/experiments/fashion/class_augmentation_plus_old_augmentation.py
+++ few_shot/experiments/fashion/class_augmentation_plus_old_augmentation.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import few_shot.experiments.fashion.config as config
 from few_shot.experiments.fashion import evaluate_fashion_few_shot
 from few_shot.dataset.fashion import fashion_dfs
-from few_shot.dataset.image_pipeline import class_augmentation_fn
+from few_shot.dataset.image_pipeline import augmented_img_pipeline_fn, class_augmentation_fn
 
 if __name__ == '__main__':
     np.random.seed(23)
@@ -23,12 +23,13 @@ if __name__ == '__main__':
                                            test_df=test_df,
                                            n_shot=n_shots,
                                            k_way_test=k_way_test,
-                                           patience=10,
+                                           patience=20,
                                            restore_best_weights=True,
+                                           img_pipeline_fn=augmented_img_pipeline_fn,
                                            post_processing_fn=class_augmentation_fn(config.K_WAY_TRAIN))
 
         results.append(result)
 
     df = pd.DataFrame.from_records(results)
     print(df)
-    df.to_csv('fashion_class_augmentation_results.csv')
+    df.to_csv('fashion_class_augmentation_plus_old_augmentation_results.csv')
